2a455d349a48da6854a1d03e0d76f358aa47ee8e
cinemapub <p.forret@brightfish.be>
2024-09-13 16:57:39 +0200

    MOD: AnalyzeDcpTest.php

--- tests/Folders/AnalyzeDcpTest.php
+++ tests/Folders/AnalyzeDcpTest.php
@@ -23,19 +23,4 @@ class AnalyzeDcpTest extends TestCase
         $this->assertNotEmpty($dcpObject->VolIndex);
     }
 
-    /**
-     * @throws InputMissingException
-     */
-    public function testWeirdCase1()
-    {
-        $dcpFolder = '/mnt/c/Users/forretp/Dropbox/Spottix/Ingest/Examples/Brightfish/DCP_LargoWinch3';
-        $dcpObject = AnalyzeDcp::simple($dcpFolder);
-        $this->assertNotEmpty($dcpObject->mxfVideo);
-        $this->assertNotEmpty($dcpObject->mxfAudio);
-        $this->assertNotEmpty($dcpObject->cplFile);
-        $this->assertNotEmpty($dcpObject->pklFile);
-        $this->assertNotEmpty($dcpObject->AssetMap);
-        $this->assertNotEmpty($dcpObject->VolIndex);
-
-    }
 }
